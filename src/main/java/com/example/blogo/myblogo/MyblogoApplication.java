eec8e0f05a77722f472a2cf67cd8c674942ea6c2
程征波 <chengzhengbo@localhost.local>
2018-07-29 14:33:25 +0800

    first

---
+++ src/main/java/com/example/blogo/myblogo/MyblogoApplication.java
@@ -0,0 +1,12 @@
+package com.example.blogo.myblogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/blogo/myblogo/MyblogoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.blogo.myblogo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyblogoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
